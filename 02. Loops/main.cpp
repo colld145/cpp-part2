9a1be1177a96076011582bec41ae5645eb90c614
Kostia <kostianikolaiev145@gmail.com>
2023-09-14 14:40:56 +0300

    Loops homework

---
+++ 02. Loops/main.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    /*Завдання 1. 
+    Вивести на екран усі числа від нуля до введеного користувачем числа*/
+
+    // int number = 0;
+    // cout << "Enter a number: ";
+    // cin >> number;
+
+    // for (int i = 0; i <= number; i++)
+    // {
+    //     cout << i << ", ";
+    // }
+    // cout << endl;
+
+    // -------------------------------------
+
+    /*Завдання 2. Користувач вводить дві межі діапазону, 
+    вивести на екран усі числа з цього діапазону. Передбачити,
+    щоб користувач міг вводити межі діапазону в довільному
+    порядку.
+        ■ вивести всі парні числа з діапазону.
+        ■ вивести всі непарні числа з діапазону.
+        ■ вивести всі числа, кратні семи.*/
+
+    // int start = 0; int finish = 0; int temp = 0;
+
+    // cout << "Enter a first number: ";
+    // cin >> start;
+    // cout << "Enter a second number: ";
+    // cin >> finish;
+    
+    // if(start > finish)
+    // {
+    //     temp = start;
+    //     start = finish;
+    //     finish = temp;
+    // }
+
+    // cout << "Start: " << start << " Finish: " << finish << endl;
+
+    // cout << "\nPair numbers: ";
+    // for (int i = start; i <= finish; i++)
+    // {
+    //     if(i % 2 == 0){
+    //         cout << i << ", ";
+    //     }
+    // }
+    // cout << endl;
+    // cout << "\nUnpair numbers: ";
+    // for (int i = start; i <= finish; i++)
+    // {
+    //     if(i % 2 != 0){
+    //         cout << i << ", ";
+    //     }
+    // }
+    // cout << endl;
+    // cout << "\nAbout 7 numbers: ";
+    // for (int i = start; i <= finish; i++)
+    // {
+    //     if(i % 7 == 0){
+    //         cout << i << ", ";
+    //     }
+    // }
+    // cout << endl;
+
+    // ----------------------------------------
+
+    /*Завдання 3. Користувач вводить дві межі діапазону. 
+    Порахувати суму всіх чисел діапазону.*/
+
+    // int start = 0; int finish = 0; int temp = 0; int sum = 0;
+
+    // cout << "Enter a first number: ";
+    // cin >> start;
+    // cout << "Enter a second number: ";
+    // cin >> finish;
+    
+    // if(start > finish)
+    // {
+    //     temp = start;
+    //     start = finish;
+    //     finish = temp;
+    // }
+
+    // cout << "Start: " << start << " Finish: " << finish << endl;
+
+    // for (int i = start; i <= finish; i++)
+    // {
+    //     sum+=i;
+    // }
+    // cout << "Sum: " << sum << endl;
+    
+    // -------------------------------------
+
+    /*Завдання 4. Користувач з клавіатури вводить числа. 
+    Порахувати їхню суму і вивести на екран, щойно користувач
+    введе нуль.*/
+    
+    // int sum = 0;
+
+    // for (int i = 0; i < 1;)
+    // {
+    //     int number = 0;
+    //     cout << "Enter a number: ";
+    //     cin >> number;
+    //     if(number == 0){
+    //         break;
+    //     }
+    //     else {
+    //         sum+= number;
+    //     }
+    // }
+    // cout << "Sum: " << sum << endl;
+
+    // --------------------------------
+
+
+
+
+    return 0;
+}
